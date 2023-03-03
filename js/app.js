194d7cde04435bc8643c647be4fd31e02ad8ae00
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-03-03 06:47:19 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -7,34 +7,61 @@ const loadData = () => {
 
 const singleData = (singleData) => {
     const cardContainer = document.getElementById('card-container')
-    singleData.forEach(singleElement => {
-        console.log(singleElement)
+    singleData.slice(0,6).forEach(singleElement => {
+        console.log(singleElement.features)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
             <img class="img-fluid p-3 rounded-5"
-              src="https://randomwordgenerator.com/img/picture-generator/chameleon-540655_640.jpg" class="card-img-top"
+              src="${singleElement.image}" class="card-img-top"
               alt="...">
             <div class="card-body">
-              <h5 class="card-title cardTitle">Card title</h5>
+              <h5 class="card-title cardTitle">Features</h5>
               <p class="card-text cardText">
-                <ol>
-                  <li>Lorem ipsum dolor sit amet.</li>
-                  <li>Aliquid magni deserunt laudantium animi!</li>
-                  <li>Facere deleniti excepturi ipsum at?</li>
-                  <li>Laboriosam molestias qui commodi quam?</li>
+                <ol id="features-list">
+                <li>${singleElement.features[0]}</li>
+                <li>${singleElement.features[1]}</li>
+                <li>${singleElement.features[2]}</li>
+                <li>${singleElement.features[3] ? singleElement.features[3] : `<pre>`}</li>
                 </ol>
               </p>
               <hr>
-              <h4 class="aiName">ChatGPT</h4>
-              <p class="aiDate"> <img class="dateIcon" src="images/calendar3.svg" alt=""> <span>11/01/2022</span></p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div>
+                  <h4 class="aiName">${singleElement.name}</h4>
+                  <p class="aiDate"> <img class="dateIcon" src="images/calendar3.svg" alt=""> <span>${singleElement.published_in}</span></p>
+               
+                 </div>
+                <div>
+                <button class="btn bg-danger-subtle
+                "><img src="images/arrow-right.svg" alt=""></button>
+               
+                </div>
+               
+              </div>
             </div>
           </div>
+          
         `
         cardContainer.appendChild(div)
         
     })
+}
+
+
+
+const featureList =  (featureList) =>{
+    // console.log(featureList[1])
+    // const featuresContainer = document.getElementById('test');
+    // featureList.forEach(singleFeatureList =>{
+    //     console.log(singleFeatureList)
+    //     const ol = document.createElement('ol')
+    //     ol.innerHTML=  `
+    //     <li>s</li>
+    //     `
+    //     document.featuresContainer.innerHTML = ol;
+    // })
 }
 
 loadData()
